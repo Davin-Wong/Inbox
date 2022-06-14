1a6fa4b4feba16ff3ace55fdeaaa85b21618e108
Davin Wong <davinlamwong@gmail.com>
2022-06-13 22:27:35 -0700

    Created and deployed simple smart contract Inbox

---
+++ README.md
@@ -0,0 +1,6 @@
+Testing a simple Solidity smart contract on the Rinkeby Test Network
+
+To Run:
+
+1. Run npm install in your terminal.
+2. Create a .env file and paste your 12 word mnemonic phrase into variable "MNEMONIC".
---
+++ test/Inbox.test.js
@@ -0,0 +1,41 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+
+const { abi, evm } = require("../compile");
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+const INITIAL_MESSAGE = "Test Msg";
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: [INITIAL_MESSAGE],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("Deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("Has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it("Can change the message", async () => {
+    const newMessage = "Give Me Ramen";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+
+    const message = await inbox.methods.message().call();
+    assert.equal(message, newMessage);
+  });
+});
